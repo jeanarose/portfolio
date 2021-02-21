5eefe6612e9a25e3bae7f77e860a740e7c1fc1bf
Jeana Rose <jr.mathis608@gmail.com>
2021-02-21 15:58:23 -0500

    Added hero image and title to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import "./Home.css"
 
 const Home = (props) => {
     return (
-        <h1 className="title">Home</h1>
+        <section class="hero is-large is-fullheight">
+        <section class="section">
+          <div class="container has-text-light has-text-centered">
+            <div class="columns is-justify-content-center">
+              <div class="column is-three-quarters hero-body">
+                <h1
+                  class="title hero-title is-size-1 has-text-light has-text-weight-bold"
+                >
+                  Hi, I'm Jeana Rose. I am a full stack developer who is open to
+                  work.
+                </h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
     );
 };
 
