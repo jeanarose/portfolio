f82d178e5fa58176ed457e39c6ba2d8253682f45
Jeana Rose <jr.mathis608@gmail.com>
2021-02-21 15:53:03 -0500

    Styled navbar menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
         <a className="navbar-item" href="/dashboard">
             <p className="is-size-4">Jeana Rose Mathis</p>
         </a>
-        <a role="button" className="navbar-burger"
+        <a role="button" className="navbar-burger mr-0"
             onClick={()=> (document.querySelector('.navbar-menu').classList.toggle('is-active'))} aria-label="menu"
             aria-expanded="false" data-target="navbarBasicExample">
             <span aria-hidden="true"></span>
@@ -16,29 +16,20 @@ const Header = (props) => {
         </a>
     </div>
     <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-            <a href="/" className="navbar-item">
+        <div className="navbar-end mr-0">
+        <a href="/" className="navbar-item has-text-right">
                 Home
             </a>
 
-            <a href="/#/portfolio" className="navbar-item">
+            <a href="/#/portfolio" className="navbar-item has-text-right">
                 Portfolio
             </a>
-            <a href="/#/contact" className="navbar-item">
+            <a href="/#/contact" className="navbar-item has-text-right">
                 Contact
             </a>
         </div>
+            
     </div>
-    {/* <div className="navbar-end">
-        <div className="navbar-item">
-            <div className="buttons">
-                <a href="/" className="button logout-button is-primary">
-                    <strong>Logout</strong>
-                </a>
-            </div>
-        </div>
-    </div> */}
-
 </nav>
     );
 };
