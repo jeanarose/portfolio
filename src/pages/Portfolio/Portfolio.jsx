5cf8593f74a89233133e9232b4eb5da0aff75c23
Jeana Rose <jr.mathis608@gmail.com>
2021-02-21 13:41:16 -0500

    Added project object and added images to the page dyamically

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,18 +1,59 @@
 import React, {useState, useEffect} from 'react';
+import "./Portfolio.css"
+import dogMeetDogImage from "../../images/dog-meet-dog.png"
+import visualizeImage from "../../images/visualize.png"
+import weatherDashboardImage from "../../images/weather-dashboard.png"
+import passwordGeneratorImage from "../../images/password-generator.png"
+import codeQuizImage from "../../images/code-quiz.png"
+import burgerLoggerImage from "../../images/burger-logger.png"
+
 
 const Portfolio = (props) => {
-    const [project, setProject] = useState(
+    const [projects, setProject] = useState([
         {
             name: "Dog Meet Dog",
             url: "https://gentle-sea-84823.herokuapp.com/",
-            github: "https://github.com/mollymccollumwx/dog-meet-dog",
-            image: "../../../images/dog-meet-dog-screenshot.png"
+            github: "https://github.com/jeanarose/dog-meet-dog",
+            image: dogMeetDogImage
+        },
+        {
+            name: "Visualize",
+            url: "https://jeanarose.github.io/vision-board/",
+            github: "https://github.com/jeanarose/vision-board",
+            image: visualizeImage
+        },
+        {
+            name: "Weather Dashboard",
+            url: "https://jeanarose.github.io/weather-dashboard",
+            github: "https://github.com/jeanarose/weather-dashboard",
+            image: weatherDashboardImage
+        },
+        {
+            name: "Password Generator",
+            url: "https://jeanarose.github.io/password-generator",
+            github: "https://github.com/jeanarose/password-generator",
+            image: passwordGeneratorImage
+        },
+        {
+            name: "Code Quiz",
+            url: "https://jeanarose.github.io/code-quiz",
+            github: "https://github.com/jeanarose/code-quiz",
+            image: codeQuizImage
+        },
+        {
+            name: "Burger Logger",
+            url: "https://dry-anchorage-08139.herokuapp.com/",
+            github: "https://github.com/jeanarose/burger-logger",
+            image: burgerLoggerImage
         }
-    )
+    ])
     return (
         <>
             <h1>Hello World</h1>
-            <img src="../../images/dog-meet-dog-screenshot.png" alt=""/>
+            {projects.map(project=>(
+            <img className="portfolio-image" src={project.image} alt=""/>
+
+            ))}
         </>
     );
 };
