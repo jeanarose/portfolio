6ed778010f6d2ff7f8770150b6c7062f550e1605
Jeana Rose <jr.mathis608@gmail.com>
2021-03-12 10:26:51 -0500

    Added hero jsx file

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = (props) => {
   });
   return (
     <section className="hero home-hero is-fullheight">
-      <div className="hero-body has-text-centered">
-        <div className="columns is-justify-content-center">
+      <div className="hero-body">
+        <div className="columns">
           <div className="column is-three-quarters hero-body">
-            <p className="title home-title is-size-1 has-text-light hax-text-weight-bold">
+            <p className="title home-title is-size-1 has-text-dark hax-text-weight-bold">
               Hi, I'm Jeana Rose. I am a full stack developer who is open to
               work.
             </p>
