6af8f326dadd3aae8341c21b6955448af5da56bb
Jeana Rose <jr.mathis608@gmail.com>
2021-03-15 09:31:34 -0400

    Created a Technologies componenet

---
+++ src/components/Technologies/Technologies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Technologies = (props) => {
+    return (
+        <div>
+            <h1 className="title is-size-1 mt-6 pt-6">Technologies</h1>
+        </div>
+    );
+};
+
+export default Technologies;
